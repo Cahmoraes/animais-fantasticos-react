7b6da009c94e4873b0372affa8a40f5489c06ac0
Caique Moraes <ike.moraes93@gmail.com>
2020-10-04 14:34:49 -0300

    animais fantasticos

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,7 +4,10 @@ import Route from './route'
 import Home from '../pages/Home'
 
 const Routes: React.FC = () => (
-  <Route path="/" exact component={Home} />
+  <>
+    <Route path="/" exact component={Home} />
+    <Route path="/Animais-Fantasticos-React/" component={Home} />
+  </>
 )
 
 export default Routes
